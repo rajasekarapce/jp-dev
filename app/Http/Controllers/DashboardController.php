23f15acce807a2578621437bee8a5a7bd0e9819f
api0707 <rajasekarapce@gmail.com>
2020-03-15 14:30:41 +0530

    skills based user searching

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,11 +7,12 @@ use App\JobApplication;
 use App\Payment;
 use App\User;
 use Illuminate\Http\Request;
+use Auth;
 use DB;
 
 class DashboardController extends Controller
 {
-    public function dashboard(){
+    public function dashboard(Request $request){
 
         $data = [
             'usersCount' => User::count(),
@@ -25,15 +26,37 @@ class DashboardController extends Controller
         ];
 
         // $applications = JobApplication::whereUserId($user_id)->orderBy('id', 'desc')->paginate(20);
-
-        $latest_jobs = DB::table('jobs')
-            //->select('*','job_applications.created_at as Applied_Date')
-            ->leftJoin('qualifications', 'jobs.qualification', '=', 'qualifications.id')
-            ->leftJoin('users', 'jobs.user_id', '=', 'users.id')
-            //->Where('job_applications.user_id', $user_id)
-            ->orderBy('jobs.id', 'desc')
-            ->paginate(10);
-            // 
+        if(Auth::user()->user_type == 'employer') {
+            $skills = $request->get('skills');
+            $latest_jobs = array();
+            if($skills){
+                $user_ids = DB::table('skills')
+                    ->join('skill_user', 'skill_user.skill_id', '=', 'skills.id')
+                    ->Where('skills.name', 'like', '%'.$skills.'%')
+                    ->select('skill_user.user_id')
+                    ->get();
+                    $userId = array();
+                    foreach($user_ids as $uid) {
+                        $userId[] = $uid->user_id;
+                    }
+                if(count($userId)){
+                    $userId = array_unique($userId);
+                    $latest_jobs = User::where('user_type', 'user')
+                        ->WhereIn('id', $userId)
+                        ->select('id','name','email','phone','updated_at')
+                        ->orderBy('id', 'desc')
+                        ->get();
+                }
+            }
+        } else {
+            $latest_jobs = DB::table('jobs')
+                //->select('*','job_applications.created_at as Applied_Date')
+                ->leftJoin('qualifications', 'jobs.qualification', '=', 'qualifications.id')
+                ->leftJoin('users', 'jobs.user_id', '=', 'users.id')
+                //->Where('job_applications.user_id', $user_id)
+                ->orderBy('jobs.id', 'desc')
+                ->paginate(10);
+        }
         return view('admin.dashboard', compact('latest_jobs','data') );
     }
 }
